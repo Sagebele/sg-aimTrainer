81e76ba9d5685df44f42ca1be305de10e3b5e615
sagebele <sdi1600109@di.uoa.gr>
2025-07-29 04:23:46 +0300

    Camera switch and Targets Spawn

--- client.lua
+++ client.lua
@@ -6,7 +6,7 @@ local pedSpawned = false
 local pedModel = Config.Locations.MainNpc.model
 local pedCoords = Config.Locations.MainNpc.coords
 local pedHeading = Config.Locations.MainNpc.heading
-
+local EXIT_KEY = Config.exitKey or 322 -- Default to 322 if not set in config
 
 
 function SpawnPed()
@@ -34,6 +34,44 @@ function SpawnPed()
     print("[sg-aimlabs] Ped created and synced.")
 end
 
+function Spawncam()
+    local cam = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
+    Config.cam = cam
+    SetCamCoord(cam, Config.Locations.Camera.coords.x, Config.Locations.Camera.coords.y, Config.Locations.Camera.coords.z)
+    PointCamAtCoord(cam, Config.Locations.Camera.lookAt.x, Config.Locations.Camera.lookAt.y, Config.Locations.Camera.lookAt.z)
+    SetCamActive(cam, true)
+    RenderScriptCams(true, true, 0, true, true)
+    print("[sg-aimlabs] Camera created.")
+
+    -- Optional: Freeze player + fade screen
+    FreezeEntityPosition(PlayerPedId(), true)
+    DoScreenFadeOut(500)
+    Wait(600)
+    DoScreenFadeIn(500)
+
+    
+end
+
+function Fixtargets()
+    -- Ensure all targets are set up correctly
+    local ped
+    for i=1, 3 do
+        ped = Config.Locations.targets["t" .. i].details.ped
+        if ped and DoesEntityExist(ped) then
+            SetBlockingOfNonTemporaryEvents(ped, true)
+            SetPedCanRagdoll(ped, true)
+            SetEntityInvincible(ped, false) -- 🔥 Now can be shot
+            SetPedDropsWeaponsWhenDead(ped, false)
+            TaskStandStill(ped, -1)
+        end
+
+
+    end
+end
+
+
+
+
 RegisterNetEvent('sg-aimlabs:spawnPed', function()
     
     SpawnPed()
@@ -54,7 +92,7 @@ CreateThread(function()
                     label = "Start Aimlabs",
                     icon = "fas fa-crosshairs",
                     action = function()
-                        TriggerEvent("sg-aimlabs:startTraining")
+                        TriggerEvent("sg-aimlabs:initializeTraining")
                     end,
                 },
             },
@@ -64,9 +102,143 @@ CreateThread(function()
 
 end)
 
-RegisterNetEvent('sg-aimlabs:startTraining', function()
+RegisterNetEvent('sg-aimlabs:initializeTraining', function()
+    TriggerEvent('sg-aimlabs:spawnTargets', source)
+    Spawncam()
+    -- Add a way to exit training
+    CreateThread(function()
+        while true do
+            Wait(10)
+            if IsControlJustPressed(0, EXIT_KEY) then 
+                print("[sg-aimlabs] Exiting training...")
+                TriggerEvent('sg-aimlabs:deleteTargets')
+                TriggerEvent("sg-aimlabs:endTrainerCam")
+                break
+            end
+            if IsControlJustPressed(0, 191) then 
+                print("[sg-aimlabs] Have fun training!")
+                TriggerEvent('sg-aimlabs:playing')
+                break
+            end
+        end
+        -- TriggerEvent("sg-aimlabs:endTrainerCam")
+    end)
+end)
+
+RegisterNetEvent('sg-aimlabs:spawnTargets', function()
     
-    print("kati")
+    local targetModel = Config.Locations.targets.t1.details.model or "a_m_y_hasjew_01"
+    local positions = {
+        Config.Locations.targets.t1.coords,
+        Config.Locations.targets.t2.coords,
+        Config.Locations.targets.t3.coords,
+    }
+    local targetHeading = Config.Locations.targets.t1.details.heading or 285.92
+    local temp = 1
+
+    RequestModel(targetModel)
+    while not HasModelLoaded(targetModel) do 
+        Wait(10) 
+    end
+
+    for _, pos in ipairs(positions) do
+        local ped = CreatePed(4, targetModel, pos.x, pos.y, pos.z, 0.0, false, true)
+        
+        SetEntityInvincible(ped, true)
+        FreezeEntityPosition(ped, true)
+        TaskStartScenarioInPlace(ped, "WORLD_HUMAN_HANG_OUT_STREET", 0, true)
+        
+        Config.Locations.targets["t" .. temp].details.ped = ped
+        temp = temp + 1
+    end
+
+end)
+
+RegisterNetEvent('sg-aimlabs:deleteTargets', function()
+    local targetPeds = {
+        Config.Locations.targets.t1.details.ped,
+        Config.Locations.targets.t2.details.ped,
+        Config.Locations.targets.t3.details.ped,
+    }
+    local tTemp = 1
+    for _, ped in ipairs(targetPeds) do
+        if DoesEntityExist(ped) then
+            DeleteEntity(ped)
+            Config.Locations.targets["t"..tTemp].details.ped = nil
+        end
+
+        tTemp = tTemp + 1
+
+    end
+
+    print("[sg-aimlabs] Targets deleted.")
 end)
 
 
+RegisterNetEvent('sg-aimlabs:endTrainerCam', function()
+    -- Destroy camera and unfreeze player
+    if Config.cam then
+        RenderScriptCams(false, true, 500, true, true)
+        DoScreenFadeOut(500)
+        Wait(600)
+        DoScreenFadeIn(500)
+        DestroyCam(Config.cam, false)
+        Config.cam = nil
+        -- Unfreeze player
+        FreezeEntityPosition(PlayerPedId(), false)
+        print("[sg-aimlabs] Camera destroyed and player unfrozen.")
+    end
+end)
+
+RegisterNetEvent('sg-aimlabs:playing', function()
+    local targets = {
+        Config.Locations.targets.t1.details.ped,
+        Config.Locations.targets.t2.details.ped,
+        Config.Locations.targets.t3.details.ped,
+    }
+    local tTemp = 1
+    local killCount = 0
+    if Config.cam then
+        RenderScriptCams(false, true, 500, true, true)
+        DoScreenFadeOut(500)
+        Wait(600)
+        DoScreenFadeIn(500)
+        DestroyCam(Config.cam, false)
+        Config.cam = nil
+        -- Unfreeze player
+        FreezeEntityPosition(PlayerPedId(), false)
+        print("[sg-aimlabs] Camera destroyed and player unfrozen.")
+    end
+    -- Unfreeze targets
+    Fixtargets()
+    CreateThread(function()
+        while true do
+            Wait(1000)        
+            print("[sg-aimlabs] Checking targets...")
+            for _, Cped in ipairs(targets) do
+                if IsPedDeadOrDying(Cped, true) and Cped ~= nil then
+                    print("[sg-aimlabs] Target killed: " .. Cped)
+                    -- Delete the target
+                    DeleteEntity(Cped)
+                    Cped = nil
+                    Config.Locations.targets["t"..tTemp].details.ped = nil
+                    
+                    -- killCount = killCount + 1
+                    -- print("[sg-aimlabs] Kill count: " .. killCount)
+                end
+                tTemp = tTemp + 1
+            end
+            if IsControlPressed(0, 73) then 
+                    print("[sg-aimlabs] Exiting training...")
+                    TriggerEvent('sg-aimlabs:deleteTargets')
+                    -- TriggerEvent("sg-aimlabs:endTrainerCam")
+                    break
+            -- elseif killCount >= 3 then
+            --     print("[sg-aimlabs] All targets killed! Training complete.")
+            --     break
+            end
+            
+            tTemp = 1 -- Reset for next check
+        end
+    end)
+end)
--- config.lua
+++ config.lua
@@ -3,7 +3,10 @@ Config = {}
 
 Config.Locations = {}
 Config.Locations.MainNpc ={}
-Config.Locations.BattleZone = {}
+Config.Locations.targets = {}
+Config.ped = nil 
+Config.cam = nil
+Config.exitKey = 322
 
 Config.Locations.MainNpc = {
     coords = vector3(1374.24, 3143.9, 39.5), 
@@ -11,10 +14,36 @@ Config.Locations.MainNpc = {
     model = "a_m_m_business_01", 
 }
 
-Config.Locations.BattleZone = {
-    coords = vector3(0, 0, 0), -- Replace with actual coordinates
-    heading = 180.0, 
-    model = "a_m_m_business_02", 
+Config.Locations.targets = {
+    t1 = {
+        coords = vector3(1316.49, 3143.13, 39.5),
+        details = {
+            model = "a_m_y_hasjew_01",
+            ped = nil, 
+            heading = 283.38,
+        }
+    }, 
+    t2 = {
+        coords = vector3(1326.54, 3149.05, 39.5),
+        details = {
+            model = "a_m_y_hasjew_01",
+            ped = nil,
+            heading = 283.38,
+        }
+    },
+    t3 = {
+        coords = vector3(1305.09, 3154.35, 39.5),
+        details = {
+            model = "a_m_y_hasjew_01",
+            ped = nil,
+            heading = 283.38,
+        }
+    },
 }
 
-Config.ped = nil 
+
+
+Config.Locations.Camera = {
+    coords = vector3(1340.46, 3152.7, 50.09), 
+    lookAt = vector3(1319.14, 3146.86, 40.41), 
+}
